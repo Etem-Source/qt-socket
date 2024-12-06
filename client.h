371a523149418fe3a865ba4feafe2c2dab960848
Etem <etemdc@gmail.com>
2024-12-06 09:57:13 +0100

    Update client.h

--- client.h
+++ client.h
@@ -1,42 +1,49 @@
 #ifndef CLIENT_H
 #define CLIENT_H
+
 #include <QDataStream>
 #include <QDialog>
 #include <QTcpSocket>
+#include <QTextEdit>
+
 QT_BEGIN_NAMESPACE
 class QComboBox;
 class QLabel;
 class QLineEdit;
 class QPushButton;
-class QTcpSocket;
 class QNetworkSession;
 QT_END_NAMESPACE
-//! [0]
+
 class Client : public QDialog
 {
-Q_OBJECT
+    Q_OBJECT
+
 public:
-explicit Client(QWidget *parent = nullptr);
+    explicit Client(QWidget *parent = nullptr);
+
 private slots:
-void requestNewFortune();
-void readFortune();
-void displayError(QAbstractSocket::SocketError socketError);
-void enableGetFortuneButton();
-void sessionOpened();
-void setupUi();
-void sendMessageToServer();
+    void displayError(QAbstractSocket::SocketError socketError);
+    void enableConnectButton();
+    void sessionOpened();
+    void setupUi();
+    void sendMessageToServer();
+    void displayMessage(const QString &sender, const QString &message);
+    void requestConnection();
+    void readMessage();
+    void onConnected();
+
 private:
-QComboBox *hostCombo = nullptr;
-QLineEdit *portLineEdit = nullptr;
-QLabel *statusLabel = nullptr;
-QPushButton *getFortuneButton = nullptr;
-QTcpSocket *tcpSocket = nullptr;
-QDataStream in;
-QString currentFortune;
-QNetworkSession *networkSession = nullptr;
-QLabel *receivedMessage;
-QLineEdit *sendMessage;
-QPushButton *sendMessageButton;
+    QComboBox *hostCombo = nullptr;
+    QLineEdit *portLineEdit = nullptr;
+    QLabel *statusLabel = nullptr;
+    QPushButton *connectButton = nullptr;
+    QTcpSocket *tcpSocket = nullptr;
+    QDataStream in;
+    QString currentFortune;
+    QNetworkSession *networkSession = nullptr;
+    QTextEdit *messageDisplay;
+    QLineEdit *sendMessage;
+    QPushButton *sendMessageButton;
 };
-//! [0]
+
 #endif
